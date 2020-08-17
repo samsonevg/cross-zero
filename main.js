9018f003348ec1cd607b215aa388dc888a9cf740
samsonevg <samsonovevgeniy28@gmail.com>
2020-08-17 21:47:03 +0500

    alert click

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-cons area = document.getElementById('area');
+const area = document.getElementById('area');
+
+area.addEventListener('click', e=> {
+    if(e.target.className = 'box') {
+        alert('123');
+    }
+})
