f6b0030767ebb203c43945702c8a520a198e752d
samsonevg <samsonovevgeniy28@gmail.com>
2020-08-20 00:58:05 +0500

    Появилось окно с объявлением игрока чей ход и счетчик остатка ходов

--- main.js
+++ main.js
@@ -2,28 +2,45 @@
 
 let boxes;
 let ifzero = false;
+let player;
+let playerStepsParagraph;
 
 window.onload = () => {
     boxes = document.getElementsByClassName("box");
+    player = document.getElementById("player");
 
+    playerStepsParagraph = document.getElementById("player__steps");
+    playerStepsParagraph.innerHTML = boxes.length;
 
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click',  (elem) => onclick(elem.currentTarget));
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('click', function(event) {
+            onClick(event.currentTarget);
+        })
     }
+
+    player.innerHTML = ' крестик';
 }
 
-function onclick(elem)
+function onClick(elem)
 {   
-    if (elem.textContent)
-        return;
-    
+    if (elem.textContent !== '')
+    return;  
+
         if (ifzero == false){
             elem.textContent = 'x';
+            player.innerHTML = ' нолик'
             ifzero = true;
          }  else {
             elem.textContent = 'o';
+            player.innerHTML = ' крестик'
             ifzero = false;
          }
-    
-   
+         minusStep();
+         
 }
+
+function minusStep()
+{
+    playerStepsParagraph.innerHTML = +playerStepsParagraph.innerHTML - 1;
+
+}   
