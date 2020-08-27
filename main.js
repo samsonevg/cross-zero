4ebeff552f375eb8f0e0476a231ee77177c92106
samsonevg <samsonovevgeniy28@gmail.com>
2020-08-28 00:15:30 +0500

    Добавил кнопку очистки игры

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ let boxes;
 let ifzero = false;
 let player;
 let playerStepsParagraph;
+let cleanButton;
 
-window.onload = function() {
+window.onload = function () {
+    cleanButton = document.getElementById("clean__button");
     boxes = document.getElementsByClassName("box");
     player = document.getElementById("player");
 
@@ -13,34 +15,45 @@ window.onload = function() {
     playerStepsParagraph.innerHTML = boxes.length;
 
     for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click', function(event) {
+        boxes[i].addEventListener('click', function (event) {
             onClick(event.currentTarget);
         })
     }
 
+    cleanButton.addEventListener("click", cleanGameField);
+
     player.innerHTML = ' крестик';
 }
 
-function onClick(elem)
-{   
+function onClick(elem) {
     if (elem.textContent !== '')
-    return;  
+        return;
 
-        if (ifzero == false){
-            elem.textContent = 'x';
-            player.innerHTML = ' нолик'
-            ifzero = true;
-         }  else {
-            elem.textContent = 'o';
-            player.innerHTML = ' крестик'
-            ifzero = false;
-         }
-         minusStep();
-         
+    if (ifzero == false) {
+        elem.textContent = 'x';
+        player.innerHTML = ' нолик'
+        ifzero = true;
+    } else {
+        elem.textContent = 'o';
+        player.innerHTML = ' крестик'
+        ifzero = false;
+    }
+    minusStep();
+
 }
 
-function minusStep()
-{
+function minusStep() {
     playerStepsParagraph.innerHTML = +playerStepsParagraph.innerHTML - 1;
 
-}   
+}
+
+function cleanGameField() {
+    //for (let i = 0; i < boxes.length; i++) { 
+    //    boxes[i].textContent = "";
+    //}
+    for (const box of boxes) { //То же самое, что и цикл выше
+        box.textContent = "";
+    }
+
+    playerStepsParagraph.innerHTML = boxes.length;
+}
